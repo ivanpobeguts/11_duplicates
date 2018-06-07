ff917574ad183dec7858b862ff85eba3aa7b217e
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-07 16:45:27 +0300

    add functions: - get_files_info - print_duplicated_files - get_parser - main add README

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+This script returns paths of duplicate files located in this specified directory. 
+'Duplicated' - are files with the same name and size. 
+
+# How to
+
+Script requires Python 3.5.
+Example of launch on Linux/Windows:
+
+``` bash
+
+$ python duplicates.py test_dir
+
+bars.json
+-- test_dir\bars.json
+-- test_dir\test_dir2\bars.json
+
+
+jtapiErrors.log
+-- test_dir\jtapiErrors.log
+-- test_dir\test_dir3\jtapiErrors.log
+
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,38 @@
+import os
+import argparse
+import collections
+
+
+def get_files_info(search_dir):
+    files_info_dict = collections.defaultdict(list)
+    for current_dir, full_dir, file_names in os.walk(search_dir):
+        for file_name in file_names:
+            full_path = os.path.join(current_dir, file_name)
+            file_size = os.path.getsize(full_path)
+            file_info = (file_name, file_size)
+            files_info_dict[file_info].append(full_path)
+    return files_info_dict
+
+
+def print_duplicated_files(files_info_dict):
+    for file, path in files_info_dict.items():
+        if len(path) > 1:
+            print(file[0])
+            for filename in path:
+                print('--', filename)
+            print('\n')
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('search_dir', help='Path to search dir')
+    return parser
+
 
 if __name__ == '__main__':
-    pass
+    try:
+        args = get_parser().parse_args()
+        files_info_dict = get_files_info(args.search_dir)
+        print_duplicated_files(files_info_dict)
+    except FileNotFoundError:
+        print('File not found.')
